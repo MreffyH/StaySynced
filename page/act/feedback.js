15812c929af9611031ccc185eafe51cc92dd0286
MreffyH <118194238+MreffyH@users.noreply.github.com>
2024-12-17 20:15:23 +0700

    Update feedback

--- page/act/feedback.js
+++ page/act/feedback.js
@@ -1,6 +1,6 @@
 import { auth } from '../../auth/firebase/config.js';
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-auth.js";
-import { getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-database.js";
+import { getDatabase, ref, set, push } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-database.js";
 
 // Initialize the database reference
 const db = getDatabase();
@@ -23,7 +23,7 @@ document.addEventListener("DOMContentLoaded", function () {
             onAuthStateChanged(auth, async (user) => {
                 if (user) {
                     const feedbackRef = ref(db, `feedback/${user.uid}`);
-                    await set(feedbackRef, {
+                    await push(feedbackRef, {
                         rating: rating,
                         feedback: feedback,
                         timestamp: new Date().toISOString()
