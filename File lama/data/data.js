2e83002a87ea0db1cdbe167bbad7b9015e66cff3
MreffyH <118194238+MreffyH@users.noreply.github.com>
2025-02-18 16:08:40 +0700

    Add docs

--- File lama/data/data.js
+++
@@ -1,5 +0,0 @@
-// Menyimpan data ke database
-import { auth } from '../auth/firebase/config.js';
-import { getDatabase, ref, set, get } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-database.js";
-
-const db = getDatabase();
